ac2c67a1c061e3414b1cedd61c78d72ac5dd94ae
randolphburt <randolph.burt@gmail.com>
2013-06-16 22:28:22 +0100

    Windows 8 Store App Implementation

---
+++ src/Windows8/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ChaseCameraWindows
+{
+    using ChaseCameraSample;
+
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            var factory = new MonoGame.Framework.GameFrameworkViewSource<ChaseCameraGame>();
+            Windows.ApplicationModel.Core.CoreApplication.Run(factory);
+        }
+    }
+}
